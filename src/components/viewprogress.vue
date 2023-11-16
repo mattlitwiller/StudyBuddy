b058e27a16d99b529dbc0129a1fe0a94ded57d3e
Ed <edouard.perrault-laliberte@mail.mcgill.ca>
2023-11-16 14:47:59 -0500

    progress stats are now in bootstrap card

--- src/components/viewprogress.vue
+++ src/components/viewprogress.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="container">
-    <button class="btn btn-secondary" @click="goBack">Back</button>
-    <h1>My Progress</h1>
-
-    <div class="progress-container">
-      <div class="progress-bar">
-        <div class="progress-item">Trainings Done ......................54</div>
-        <div class="progress-item">Questions Answered...................202</div>
-        <div class="progress-item">Good answer of first try.............78</div>
-        <div class="progress-item">Questions created....................78</div>
-        <div class="progress-item">Average Grade........................77%</div>
-        <div class="progress-item">Time spent on training...............5h 25</div>
+  <div>
+    <button class="btn btn-secondary" style="margin-bottom: 20px;" @click="goBack">Back</button>
+    <div class="card bg-primary text-white" style="width:400px; margin: auto;">
+      <div class="card-header">My Progress</div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item bg-transparent">Trainings Done ......................54</li>
+          <li class="list-group-item bg-transparent">Questions Answered...................202</li>
+          <li class="list-group-item bg-transparent">Good answer of first try.............78</li>
+          <li class="list-group-item bg-transparent">Questions created....................78</li>
+          <li class="list-group-item bg-transparent">Average Grade........................77%</li>
+          <li class="list-group-item bg-transparent">Time spent on training...............5h25</li>
+        </ul>
       </div>
     </div>
   </div>
