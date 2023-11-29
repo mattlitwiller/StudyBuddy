fc67527fafa48266464d52176218098b189a6da4
mehdilimitless <mammor@student.unimelb.edu.au>
2023-11-29 10:52:28 -0500

    fixed

--- src/components/createquestions.vue
+++ src/components/createquestions.vue
@@ -110,9 +110,19 @@ export default {
     this.newQuestion = { text: '', answer: '', hint: '',deck: this.newQuestion.deck }; // Reset with the first deck as default
     this.showCongratsModal = true;
   } else {
-    alert("Please fill in all fields before adding a question.");
+    alert("Please fill in question and answer fields before adding a question.");
   }
 },
+handleMouseOver() {
+    if(!this.newQuestion.text || !this.newQuestion.answer){
+      this.errorContent = "Question and answer fields are mandatory"
+      console.log(this.errorContent)
+    }
+  },
+  handleMouseLeave() {
+    this.errorContent = ""
+    console.log(this.errorContent)
+  },
 
     closeModal() {
       this.showCongratsModal = false;
